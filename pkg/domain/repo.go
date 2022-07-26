0d2198048d9b5513f0ff4aa2c11f661e73d36960
M-Faraz3110 <faraz551@live.com>
2022-07-26 12:57:25 +0400

    lmao

--- pkg/domain/repo.go
+++ pkg/domain/repo.go
@@ -7,7 +7,6 @@ import (
 	"rabbitmqtest/pkg/infra/config"
 	"runtime"
 	"strconv"
-	"sync"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/data/aztables"
 	"github.com/BetaLixT/usago"
@@ -19,13 +18,7 @@ type RabbitMqRepository struct {
 	client *aztables.Client
 }
 
-func (repo *RabbitMqRepository) Sub(logger *zap.Logger, topic string) string {
-	_, err := repo.client.CreateTable(context.TODO(), nil)
-	if err != nil {
-		fmt.Println("Table already Created")
-	} else {
-		fmt.Println("Table created")
-	}
+func (repo *RabbitMqRepository) Channel(logger *zap.Logger, topic string) {
 	manager := usago.NewChannelManager("amqp://guest:guest@localhost:55001/", logger)
 	bldr := usago.NewChannelBuilder().WithQueue(
 		topic,
@@ -37,8 +30,10 @@ func (repo *RabbitMqRepository) Sub(logger *zap.Logger, topic string) string {
 	).WithConfirms(true)
 	chnl, err := manager.NewChannel(*bldr)
 	if err != nil {
+		_, src, line, _ := runtime.Caller(0)
+		repo.Log("", "", src, line, logger, "failed to create channel")
 		fmt.Printf("failed to create channel")
-		return "FAIL"
+		return
 	}
 	consumer, _ := chnl.RegisterConsumer(
 		topic,
@@ -50,54 +45,67 @@ func (repo *RabbitMqRepository) Sub(logger *zap.Logger, topic string) string {
 		nil,
 	)
 	fmt.Println("CONSUMER REGISTERED")
-	wg := sync.WaitGroup{}
-	wg.Add(1)
+	msg := <-consumer
+	_, src, line, _ := runtime.Caller(0)
+	repo.Log(string(msg.Body), msg.RoutingKey, src, line, logger, "message read")
+}
+
+func (repo *RabbitMqRepository) Log(body string, routingKey string, src string, line int, logger *zap.Logger, message string) {
 	count := 1
 
-	go func() {
-		defer wg.Done()
-		msg := <-consumer
-		observedZapCore, observedLogs := observer.New(zap.InfoLevel)
-		observedLogger := zap.New(observedZapCore)
-		observedLogger.Info(
-			"message read",
-			zap.String("body", string(msg.Body)),
-		)
-		logbody := observedLogs.All()[0]
-		_, src, line, _ := runtime.Caller(0)
-		log := RabbitMq{
-			Entity: aztables.Entity{
-				PartitionKey: "1",
-				RowKey:       strconv.Itoa(count),
-			},
-			Level:      logbody.Level.String(),
-			Ts:         strconv.Itoa(int(logbody.Time.Unix())),
-			Caller:     src + ":" + strconv.Itoa(line),
-			Msg:        logbody.Message,
-			Body:       string(msg.Body),
-			RoutingKey: string(msg.RoutingKey),
-		}
-		fmt.Println(log)
-		marshalled, err := json.Marshal(log)
-		if err != nil {
-			panic(err)
-		}
-		_, err = repo.client.AddEntity(context.TODO(), marshalled, nil) // TODO: Check access policy, need Storage Table Data Contributor role
-		if err != nil {
-			panic(err)
-		}
-		// for err2 != nil {
-		// 	count = count + 1
-		// 	log.RowKey = strconv.Itoa(count)
-		// 	marshalled, err := json.Marshal(log)
-		// 	if err != nil {
-		// 		panic(err)
-		// 	}
-		// 	_, err2 = repo.client.AddEntity(context.TODO(), marshalled, nil) // TODO: Check access policy, need Storage Table Data Contributor role
-		// }
+	observedZapCore, observedLogs := observer.New(zap.InfoLevel)
+	observedLogger := zap.New(observedZapCore)
+	observedLogger.Info(
+		message,
+		zap.String("body", string(body)),
+	)
+	logbody := observedLogs.All()[0]
 
-	}()
-	wg.Wait()
+	log := RabbitMq{
+		Entity: aztables.Entity{
+			PartitionKey: "1",
+			RowKey:       strconv.Itoa(count),
+		},
+		Level:      logbody.Level.String(),
+		Ts:         strconv.Itoa(int(logbody.Time.Unix())),
+		Caller:     src + ":" + strconv.Itoa(line),
+		Msg:        logbody.Message,
+		Body:       string(body),
+		RoutingKey: string(routingKey),
+	}
+	fmt.Println(log)
+	marshalled, err := json.Marshal(log)
+	if err != nil {
+		panic(err)
+	}
+	_, err = repo.client.AddEntity(context.TODO(), marshalled, nil) // TODO: Check access policy, need Storage Table Data Contributor role
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func (repo *RabbitMqRepository) Table(logger *zap.Logger, topic string) {
+	_, err := repo.client.CreateTable(context.TODO(), nil)
+	if err != nil {
+		fmt.Println("Table already Created")
+	} else {
+		fmt.Println("Table created")
+	}
+}
+
+func (repo *RabbitMqRepository) Sub(logger *zap.Logger, topic string) string {
+	repo.Table(logger, topic)
+	repo.Channel(logger, topic)
+	// for err2 != nil {
+	// 	count = count + 1
+	// 	log.RowKey = strconv.Itoa(count)
+	// 	marshalled, err := json.Marshal(log)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	_, err2 = repo.client.AddEntity(context.TODO(), marshalled, nil) // TODO: Check access policy, need Storage Table Data Contributor role
+	// }
 	return "SUCCESS"
 
 }
